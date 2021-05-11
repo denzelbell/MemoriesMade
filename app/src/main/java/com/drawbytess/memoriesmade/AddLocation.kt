75aec9691f31cfdca28090218cb441db12b66344
denzelbell <dnzlbell@gmail.com>
2021-05-11 17:18:38 -0500

    - Updated date format for view

--- app/src/main/java/com/drawbytess/memoriesmade/AddLocation.kt
+++ app/src/main/java/com/drawbytess/memoriesmade/AddLocation.kt
@@ -1,21 +1,65 @@
 package com.drawbytess.memoriesmade
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_add_location.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddLocation : AppCompatActivity() {
+class AddLocation : AppCompatActivity(), View.OnClickListener {
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_location)
 
         // Set up toolbar
         setSupportActionBar(toolb_add_place)
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toolb_add_place.setNavigationOnClickListener {
             onBackPressed()
         }
 
+        dateSetListener = DatePickerDialog.OnDateSetListener {
+            view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView() // Updates the view once the date dialog is closed
+        }
+        et_date.setOnClickListener(this)
     }
+
+    /**
+     * Set up DatePicker to popup when id et_date is selected
+     */
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.et_date -> {
+                DatePickerDialog(
+                        this@AddLocation,
+                        dateSetListener,
+                        cal.get(Calendar.YEAR),
+                        cal.get(Calendar.MONTH),
+                        cal.get(Calendar.DAY_OF_MONTH))
+                        .show()
+            }
+        }
+    }
+    /**
+     * Places date selected in date picker in text field
+     */
+    private fun updateDateInView(){
+        val myFormat = "MM/dd/yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        et_date.setText(sdf.format(cal.time).toString())
+    }
+
+
 }
