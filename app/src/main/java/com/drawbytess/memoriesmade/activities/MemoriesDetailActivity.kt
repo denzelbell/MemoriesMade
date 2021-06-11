0ac8592b7ff28e21151b81c839833676a4d7cf3d
denzelbell <dnzlbell@gmail.com>
2021-06-11 09:39:50 -0500

    - Added toolbar and back button

---
+++ app/src/main/java/com/drawbytess/memoriesmade/activities/MemoriesDetailActivity.kt
@@ -0,0 +1,21 @@
+package com.drawbytess.memoriesmade.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.drawbytess.memoriesmade.R
+import kotlinx.android.synthetic.main.activity_add_location.*
+import kotlinx.android.synthetic.main.activity_memories_detail.*
+
+class MemoriesDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memories_detail)
+
+        // Set up toolbar
+        setSupportActionBar(toolbar_memories_detail)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar_add_place.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
