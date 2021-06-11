6afd9f8e09f288815d3803ef7661566df8d152fe
denzelbell <dnzlbell@gmail.com>
2021-06-10 20:19:48 -0500

    - Testing the database with the Log.

--- app/src/main/java/com/drawbytess/memoriesmade/activities/MainActivity.kt
+++ app/src/main/java/com/drawbytess/memoriesmade/activities/MainActivity.kt
@@ -3,7 +3,10 @@ package com.drawbytess.memoriesmade.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.drawbytess.memoriesmade.R
+import com.drawbytess.memoriesmade.database.DatabaseHandler
+import com.drawbytess.memoriesmade.models.MemoriesModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,5 +22,22 @@ class MainActivity : AppCompatActivity() {
                     AddLocation::class.java)
             startActivity(intent)
         }
+
+        getMemoryListFromLocalDB()
+    }
+
+    private fun getMemoryListFromLocalDB(){
+        val dbHandler = DatabaseHandler(this)
+        val getMemList: ArrayList<MemoriesModel> = dbHandler.getMemoriesList()
+
+        if (getMemList.size > 0){
+            for (i in getMemList){
+                Log.e("Title", i.title)
+                Log.e("Description", i.description)
+                Log.e("Date", i.date)
+                Log.e("Image", i.image)
+            }
+        }
+
     }
 }
