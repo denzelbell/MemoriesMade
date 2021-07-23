7fbfc572e1b3f22c2da33451b85ebf2bc6b2bba7
denzelbell <dnzlbell@gmail.com>
2021-07-23 17:17:40 -0500

    - setup recyclerview and toolbar

--- app/src/main/java/com/drawbytess/memoriesmade/activities/MemoriesDetailActivity.kt
+++ app/src/main/java/com/drawbytess/memoriesmade/activities/MemoriesDetailActivity.kt
@@ -1,8 +1,10 @@
 package com.drawbytess.memoriesmade.activities
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.drawbytess.memoriesmade.R
+import com.drawbytess.memoriesmade.models.MemoriesModel
 import kotlinx.android.synthetic.main.activity_memories_detail.*
 
 class MemoriesDetailActivity : AppCompatActivity() {
@@ -10,13 +12,25 @@ class MemoriesDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memories_detail)
 
+        var memoryPlaceDetailModel : MemoriesModel? = null
 
-        // Set up toolbar
-        setSupportActionBar(toolbar_memories_detail)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        toolbar_memories_detail.setNavigationOnClickListener {
-            onBackPressed()
+        if(intent.hasExtra(MainActivity.EXTRA_PLACE_DETAILS)) {
+            memoryPlaceDetailModel =
+                intent.getParcelableExtra(MainActivity.EXTRA_PLACE_DETAILS) as MemoriesModel
         }
 
+        if (memoryPlaceDetailModel != null) {
+            setSupportActionBar(toolbar_memories_detail)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.title = memoryPlaceDetailModel.title
+
+            toolbar_memories_detail.setNavigationOnClickListener {
+                onBackPressed()
+            }
+
+            iv_place_image.setImageURI(Uri.parse(memoryPlaceDetailModel.image))
+            tv_description.text = memoryPlaceDetailModel.description
+            tv_location.text = memoryPlaceDetailModel.location
+        }
     }
 }
